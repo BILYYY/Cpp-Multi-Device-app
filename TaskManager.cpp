0e0d9be7d8ab658ede23e5c346a97cc8f621ebbf
ilyass bouchabti <63551022+BILYYY@users.noreply.github.com>
2023-12-31 03:55:03 +0100

    Update TaskManager.cpp

--- TaskManager.cpp
+++ TaskManager.cpp
@@ -9,37 +9,25 @@
 #pragma resource "*.fmx"
 TForm1 *Form1;
 //---------------------------------------------------------------------------
-__fastcall TForm1::TForm1(TComponent* Owner)
-	: TForm(Owner)
-{
-}
+__fastcall TForm1::TForm1(TComponent* Owner):TForm(Owner){}
 //---------------------------------------------------------------------------
-void __fastcall TForm1::AddTaskButtonClick(TObject *Sender)
-{
-	AddTask();
-}
-
+void __fastcall TForm1::AddTaskButtonClick(TObject *Sender){AddTask();}
 //---------------------------------------------------------------------------
  void __fastcall TForm1::AddTask(){
 	String taskText = TaskInput->Text.Trim();
-	if (!taskText.IsEmpty()) {
+	if (!taskText.IsEmpty()){
 		TasksListBox->Items->Add(taskText);
 		TaskInput->Text="";
 	}
  }
-
 //---------------------------------------------------------------------------
-void __fastcall TForm1::TasksListBoxItemClick(TCustomListBox * const Sender, TListBoxItem * const Item)
-
-{
+void __fastcall TForm1::TasksListBoxItemClick(TCustomListBox * const Sender, TListBoxItem * const Item){
 	TasksListBox->Items->Delete(Item->Index);
 }
 //---------------------------------------------------------------------------
 
 //---------------------------------------------------------------------------
-void __fastcall TForm1::TaskInputKeyDown(TObject *Sender, WORD &Key, System::WideChar &KeyChar,
-          TShiftState Shift)
-{
+void __fastcall TForm1::TaskInputKeyDown(TObject *Sender, WORD &Key, System::WideChar &KeyChar,TShiftState Shift){
 	if(Key == 13){
 		AddTask();
 	}
